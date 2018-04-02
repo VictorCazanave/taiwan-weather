07f35afb090621bfe2989f15d286967e7065b055
Victor Cazanave <victor.cazanave@gmail.com>
2018-04-02 22:22:27 +0800

    [UT] Try but fail to fix files-utils tests

--- test/files-utils.spec.js
+++ test/files-utils.spec.js
@@ -1,64 +1,75 @@
-const chai = require('chai')
+const chai = require('chai');
 const sinon = require('sinon');
-const sinonChai = require("sinon-chai");
+const sinonChai = require('sinon-chai');
 const fs = require('fs');
 const stream = require('stream');
+const unzipper = require('unzipper');
 const fu = require('../lib/files-utils');
 const expect = chai.expect;
 chai.use(sinonChai);
 
 describe('Files Utils', () => {
-
 	describe('#getFiles', () => {
-
 		describe('With invalid options', () => {
-			let invalidOpt = null;
-
-			beforeEach(() => {
-				invalidOpt = {
+			it('should call callback function with error', done => {
+				const invalidOpt = {
 					output: 123 // Number instead of String
-				}
-			});
+				};
 
-			afterEach(() => {});
-
-			it('should call callback function with error', (done) => {
-				fu.getFiles(null, invalidOpt, (err) => {
-					expect(err).not.to.be.null;
+				fu.getFiles(null, invalidOpt, err => {
 					expect(err instanceof Error).to.be.true;
 
 					done();
 				});
 			});
-
 		});
 
+		// TODO
+		xdescribe('With no option', () => {});
+
+		//TODO: Fix problem with mockedStream
 		describe('With valid options', () => {
 			let mockedStream = null;
 			let opt = null;
 			let existsSync = null;
 			let mkdirSync = null;
+			let mockParse = null;
+			let mockDuplex = null;
+			let newParse = null;
 
 			beforeEach(() => {
 				mockedStream = new stream.Readable({
 					objectMode: true,
-					read: function (size) {}
+					read: size => {
+						console.log('API STREAM READ');
+						return null;
+					}
 				});
 				opt = {};
 				existsSync = sinon.stub(fs, 'existsSync');
 				mkdirSync = sinon.stub(fs, 'mkdirSync');
+				mockParse = sinon.stub(unzipper, 'Parse');
+				mockDuplex = new stream.Duplex();
+				newParse = new unzipper.Parse();
 			});
 
-			afterEach(() => {});
-			
-			//TODO: Fix problem with mockedStream
-			xit('should create output dir when does not exist', (done) => {
+			afterEach(() => {
+				existsSync.restore();
+				mkdirSync.restore();
+			});
+
+			it('should create output dir when does not exist', done => {
 				existsSync.returns(false);
+				//mockParse.returns(mockDuplex);
+				console.log('111');
 
 				fu.getFiles(mockedStream, opt, () => {
+					console.log('222');
 					expect(mkdirSync).to.have.been.calledWith(opt.output);
 
+					mockedStream._readableState.ended = true;
 					mockedStream.emit('finish');
+					mockedStream.emit('emd');
 					done();
 				});
 			});
